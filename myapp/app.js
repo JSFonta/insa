09210b5a3ac0bacc87fe4d341490608fe3771b9a
JSFonta <jeanseb.fonta@gmail.com>
2018-05-17 11:52:18 +0100

    Ajout de pug

--- myapp/app.js
+++ myapp/app.js
@@ -1,19 +1,14 @@
 const express = require('express')
 const app = express()
-let router = express.Router();
 
-// middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.set('view engine', 'pug')
 
 app.get('/flo', function (req, res) {
   res.send('Hello Flo')
+})
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!', paragraphe:'Florian tu es gentil' })
 })
 
 app.listen(8123, function () {
