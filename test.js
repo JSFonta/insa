cafd28bc960b604e37b3fccdd680ffaa1f38f9ca
JSFonta <jeanseb.fonta@gmail.com>
2018-05-17 10:54:05 +0100

    Demarage de node

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ const net = require('net');
 
 console.log('Serveur démarré');
 
-while(1) {
+var server = net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+	socket.pipe(socket);
+});
 
-	
-	
-}
+server.listen(8123, '127.0.0.1');
