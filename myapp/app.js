242beaad09ffd716f7364f9284c9f8e067c23a5a
JSFonta <jeanseb.fonta@gmail.com>
2018-05-17 11:03:02 +0100

    Serveur Flo

--- myapp/app.js
+++ myapp/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/flo', function (req, res) {
+  res.send('Hello Flo')
+})
+
 app.listen(8123, function () {
   console.log('Example app listening on port 8123!')
 })
